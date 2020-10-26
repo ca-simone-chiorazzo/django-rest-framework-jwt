e8327641383f52d44a6758c50fae386838c42b7d
Simone Chiorazzo <simone.chiorazzo@cloudacademy.com>
2020-10-26 12:03:50 +0100

    Restructured library settings to manage multiple issuers and expose the old API for backwards compatibility

--- src/rest_framework_jwt/settings.py
+++ src/rest_framework_jwt/settings.py
@@ -4,12 +4,15 @@ from __future__ import unicode_literals
 
 import datetime
 
+import six
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from rest_framework.settings import APISettings
+
+from rest_framework_jwt.multi_issuer_api_settings import DEFAULT_ISSUER_CODE, MultiIssuerAPISettings
 
 USER_SETTINGS = getattr(settings, 'JWT_AUTH', None)
 
+# List of settings that will be injected as defaults for each jwt issuer
 DEFAULTS = {
     'JWT_SECRET_KEY': settings.SECRET_KEY,
     'JWT_GET_USER_SECRET_KEY': None,
@@ -45,6 +48,7 @@ DEFAULTS = {
     'JWT_AUTH_COOKIE_SAMESITE': 'Lax',
     'JWT_IMPERSONATION_COOKIE': None,
     'JWT_DELETE_STALE_BLACKLISTED_TOKENS': False,
+
 }
 
 # List of settings that may be in string import notation.
@@ -57,23 +61,33 @@ IMPORT_STRINGS = (
     'JWT_GET_USER_SECRET_KEY',
 )
 
-api_settings = APISettings(USER_SETTINGS, DEFAULTS, IMPORT_STRINGS)
+issuer_registry = MultiIssuerAPISettings()
+
+if USER_SETTINGS and DEFAULT_ISSUER_CODE in USER_SETTINGS:
+    for issuer_id, issuer_user_settings in six.iteritems(USER_SETTINGS):
+        issuer_registry.add_issuer_settings(issuer_id, defaults=DEFAULTS, user_settings=issuer_user_settings,
+            import_from_strings=IMPORT_STRINGS, )
+else:
+    issuer_registry.add_issuer_settings(DEFAULT_ISSUER_CODE, defaults=DEFAULTS, user_settings=USER_SETTINGS,
+        import_from_strings=IMPORT_STRINGS, )
+
+api_settings = issuer_registry
 
 # check if settings have valid values
-if not isinstance(api_settings.JWT_EXPIRATION_DELTA, datetime.timedelta):  # pragma: no cover
+for jwt_auth_config in api_settings.get_issuer_settings_registry().values():
+    if not isinstance(jwt_auth_config.JWT_EXPIRATION_DELTA, datetime.timedelta):  # pragma: no cover
+        raise ImproperlyConfigured(
+            '`JWT_EXPIRATION_DELTA` setting must be instance of '
+            '`datetime.timedelta`')
 
-    raise ImproperlyConfigured(
-        '`JWT_EXPIRATION_DELTA` setting must be instance of '
-        '`datetime.timedelta`')
+    if not isinstance(
+            jwt_auth_config.JWT_REFRESH_EXPIRATION_DELTA, datetime.timedelta):  # pragma: no cover
 
-if not isinstance(
-        api_settings.JWT_REFRESH_EXPIRATION_DELTA, datetime.timedelta):  # pragma: no cover
+        raise ImproperlyConfigured(
+            '`JWT_REFRESH_EXPIRATION_DELTA` setting must be instance of '
+            '`datetime.timedelta`')
 
-    raise ImproperlyConfigured(
-        '`JWT_REFRESH_EXPIRATION_DELTA` setting must be instance of '
-        '`datetime.timedelta`')
-
-if api_settings.JWT_TOKEN_ID not in {'off', 'include', 'require'}:
-    raise ImproperlyConfigured(
-        "`JWT_TOKEN_ID` setting must be 'off', 'include' or 'require'"
-    )
+    if jwt_auth_config.JWT_TOKEN_ID not in {'off', 'include', 'require'}:
+        raise ImproperlyConfigured(
+            "`JWT_TOKEN_ID` setting must be 'off', 'include' or 'require'"
+        )
